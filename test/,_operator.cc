d2984d61888f47374cad27d19ac6a182a308ef43
bfzq <bigbuggiggle@hotmail.com>
2020-01-22 17:48:26 +0800

    NA

--- test/,_operator.cc
+++ test/,_operator.cc
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main()
 {
-  /*
-    逗号运算符的优先级低于赋值操作符
-   */
   // int a = 1,2; // this is error
+  1; // this is ok
+  (1,2); // this is ok
+  1,2; // this is ok
   int a = (1,2); // this is ok
   printf("%d", a); // result is 2
+  a = 1,2;// this is ok
+  printf("%d", a); // result is 1
   return 0;
 }
