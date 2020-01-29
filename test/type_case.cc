fe4da01deb35a241a7b3632d7c7336bb760d77c0
bfzq <bigbuggiggle@hotmail.com>
2020-01-29 16:55:43 +0800

    NA

--- test/type_case.cc
+++ test/type_case.cc
@@ -6,5 +6,15 @@ int main()
   int b = int(a);
   std::cout << std::bitset<sizeof(double) * 8>((*(int *)&a)) << std::endl;
   std::cout << std::bitset<sizeof(int) * 8>(b) << std::endl;
+
+  std::cout << sizeof(short) * 8 << std::endl;
+  unsigned short c = 0xffff;
+  short d = c;
+  std::cout << c << std::endl;
+  std::cout << d << std::endl;
+
+  int e = -1;
+  unsigned int f = 1;
+  std::cout << std::hex << e - f << std::endl;
   return 0;
 }
