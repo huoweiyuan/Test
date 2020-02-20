4640964619cb6f3df9a63e184a4fc6b7f8d7c57a
bfzq <bigbuggiggle@hotmail.com>
2020-02-20 10:53:11 +0800

    sizeof bitset

--- test/bit.cc
+++ test/bit.cc
@@ -2,6 +2,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <bitset>
+using namespace std;
+#define bit_1_mask (0xffffffffffffffff)
+#define bit_2_mask (bit_1_mask << 64)
 int main()
 {
   // 位域
@@ -17,6 +20,20 @@ int main()
   std::cout << std::bitset<8>(block) << std::endl;
   block = block | 0x8;
   std::cout << std::bitset<8>(block) << std::endl;
-  printf("%x\n", block);
+  printf("%x\n====\n", block);
+  bitset<65> bitco1;
+  bitset<8 * 4> bitco2;
+  bitset<8 * 1024> bitco3;
+  cout << sizeof(bitco1) << endl;
+  cout << sizeof(bitco2) << endl;
+  cout << sizeof(bitco3) << endl;
+  bitset<512> bitco4;
+  unsigned long bit_long = bitco4.to_ulong();
+  cout << sizeof(bit_long) << " " << sizeof(bitco4) << endl;
+  // cout << bitco4.to_string() << endl;
+  bitset<1024> bit5(bit_1_mask);
+  cout << bit5.to_string() << endl;
+  bit5 <<= 64;
+  cout << bit5.to_string() << endl;
   return 0;
 }
