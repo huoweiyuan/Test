a836b9f7861166a0f2734bfbf9f62ec70221f597
bfzq <bigbuggiggle@hotmail.com>
2020-03-15 18:44:37 +0800

    avl insert init

--- test/avl.h
+++ test/avl.h
@@ -1,23 +1,33 @@
 #ifndef _AVL_H_
 #define _AVL_H_
-
+#include <stdlib.h>
 template<typename T>
-struct tree_node_struct
+struct avl_node_struct
 {
   T element;
-  int bf;
-  struct tree_node_struct *left;
-  struct tree_node_struct *right;
+  unsigned int height;
+  struct avl_node_struct *left;
+  struct avl_node_struct *right;
 };
 
+template<typename T>
+void avl_node_struct_init(avl_node_struct<T> *node)
+{
+  node->height = 0;
+  node->left = nullptr;
+  node->right = nullptr;
+}
 template<typename E>
 class avl
 {
-private:
-  struct tree_node_struct<E> *root;
-public:
+ private:
+  struct avl_node_struct<E> *root;
+ public:
   int erase(const E&);
-  void insert(const E&);
+  int insert(const E&);
+ private:
+  struct avl_node_struct<E>* avl_insert(struct avl_node_struct<E> *tree,
+                                        const E&);
 };
 
 template<typename E>
@@ -27,8 +37,32 @@ int avl<E>::erase(const E& e)
 }
 
 template<typename E>
-void avl<E>::insert(const E& e)
+struct avl_node_struct<E>* avl<E>::avl_insert(struct avl_node_struct<E> *tree,
+                                      const E &e)
 {
-  
+  if (tree == nullptr)
+  {
+    tree = (avl_node_struct<E>*)malloc(sizeof(avl_node_struct<E>));
+    avl_node_struct_init<E>(tree);
+    tree->element = e;
+    return tree;
+  }
+  else
+  {
+    // TODO
+  }
+  return tree;
+}
+
+template<typename E>
+int avl<E>::insert(const E& e)
+{
+  struct avl_node_struct<E> *tree = nullptr;
+  tree = avl_insert(root, e);
+  if (tree == nullptr)
+    return -1;
+  else
+    root = tree;
+  return 0;
 }
 #endif // _AVL_H_
