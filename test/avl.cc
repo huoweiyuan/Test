e06ca739bef1263c703dbecba27f02f2f82bb986
bfzq <bigbuggiggle@hotmail.com>
2020-03-18 11:55:03 +0800

    print avl

--- test/avl.cc
+++ test/avl.cc
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "avl.h"
 #include "print_tree.h"
+void visit(const avl_node_struct<char> *tree)
+{
+  printf("%c\n", tree->element);
+}
 void test1()
 {
   avl<char> a;
   a.insert('a');
   a.insert('b');
   a.insert('c');
-  printf("%d", tree_height<avl_node_struct<char>>(get_tree(a)));
+  printf("%d\n", tree_height<avl_node_struct<char>>(get_tree(a)));
+  print_tree<avl_node_struct<char>>(get_tree(a), visit);
 }
 int main()
 {
