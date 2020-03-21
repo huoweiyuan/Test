ade1efc034a32e3dd5ed28bd264edaff801b2f98
bfzq <bigbuggiggle@hotmail.com>
2020-03-21 18:21:20 +0800

    add test3

--- test/avl.cc
+++ test/avl.cc
@@ -5,6 +5,8 @@ void visit(const avl_node_struct<char> *tree)
 {
   printf("%c\n", tree->element);
 }
+
+// test insert
 void test1()
 {
   Avl<char> a;
@@ -24,6 +26,8 @@ void visit2(const avl_node_struct<int> *tree)
 {
   printf("%d ", tree->element);
 }
+
+// test erase
 void test2()
 {
   Avl<int> a;
@@ -46,11 +50,30 @@ void test2()
   printf("%d\n", tree_height<avl_node_struct<int>>(get_tree(a)));
   print_tree<avl_node_struct<int>>(get_tree(a), visit2);
   printf("\n");
+  a.erase(3);
+  printf("%d\n", tree_height<avl_node_struct<int>>(get_tree(a)));
+  print_tree<avl_node_struct<int>>(get_tree(a), visit2);
+  printf("\n");
+}
+
+void test3()
+{
+  Avl<int> a;
+  a.insert(3);
+  a.insert(2);
+  a.insert(1);
+  a.insert(6);
+  a.insert(5);
+  a.insert(8);
+  a.erase(3);
+  printf("%d\n", tree_height<avl_node_struct<int>>(get_tree(a)));
+  print_tree<avl_node_struct<int>>(get_tree(a), visit2);
+  printf("\n");
 
 }
 int main()
 {
   // test1();
-  test2();
+  test3();
   return 0;
 }
