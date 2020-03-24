b4b4ec928b5f6b8cab9700373c3230ced9769a53
bfzq <bigbuggiggle@hotmail.com>
2020-03-24 23:46:35 +0800

    test1

--- test/rbtree.cc
+++ test/rbtree.cc
@@ -2,9 +2,24 @@
 #include "rbtree.h"
 #include "print_tree.h"
 
+using bfzq::rb_node_struct;
+using bfzq::RBTree;
+
+void visit1(const rb_node_struct<int> *tree)
+{
+  printf(".element=%d .color=%d", tree->element, tree->color);
+}
+
 void test1()
 {
   printf("test1\n");
+  RBTree<int> *test = new RBTree<int>();
+  test->insert(3);
+  test->insert(2);
+  test->insert(1);
+  print_tree<rb_node_struct<int>>(get_tree(*test), visit1);
+  printf("\n");
+  delete test;
 }
 
 int main()
