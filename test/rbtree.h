723ae446f1edda94a1526bbb08b742637b579ea5
bfzq <bigbuggiggle@hotmail.com>
2020-03-24 21:46:03 +0800

    定义节点结构

--- test/rbtree.h
+++ test/rbtree.h
@@ -1,7 +1,21 @@
 #ifndef _RBTREE_H_
 #define _RBTREE_H_
-class RBTree
+namespace bfzq
 {
-  
+  enum Color {RED, BLACK};
+  template<typename E>
+  struct rb_node_struct
+  {
+    E element;
+    Color color;
+    struct rb_node_struct *left;
+    struct rb_node_struct *right;
+  };
+  class RBTree
+  {
+ public:
+ private:
+    
+  };
 };
 #endif // _RBTREE_H_
