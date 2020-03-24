631e7d368ca496498c1c615d33dd6fe299815103
bfzq <bigbuggiggle@hotmail.com>
2020-03-24 21:58:22 +0800

    NA

--- test/rbtree.h
+++ test/rbtree.h
@@ -2,20 +2,20 @@
 #define _RBTREE_H_
 namespace bfzq
 {
-  enum Color {RED, BLACK};
-  template<typename E>
-  struct rb_node_struct
-  {
-    E element;
-    Color color;
-    struct rb_node_struct *left;
-    struct rb_node_struct *right;
-  };
-  class RBTree
-  {
+enum Color {RED, BLACK};
+template<typename E>
+struct rb_node_struct
+{
+  E element;
+  Color color;
+  struct rb_node_struct *left;
+  struct rb_node_struct *right;
+};
+class RBTree
+{
  public:
  private:
-    
-  };
+  
+};
 };
 #endif // _RBTREE_H_
