51885a0ce74da59b86c81ebbe3ebe6f15c90a93c
bfzq <bigbuggiggle@hotmail.com>
2020-03-24 21:28:33 +0800

    add test1

---
+++ test/rbtree.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "rbtree.h"
+void test1()
+{
+  printf("test1\n");
+}
+
+int main()
+{
+  test1();
+  return 0;
+}
---
+++ test/rbtree.h
@@ -0,0 +1,7 @@
+#ifndef _RBTREE_H_
+#define _RBTREE_H_
+class RBTree
+{
+  
+};
+#endif // _RBTREE_H_
