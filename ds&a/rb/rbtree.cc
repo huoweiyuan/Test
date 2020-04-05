9e1aa9ad38101ed00f75e1dc76429eabd3b13d79
bfzq <bigbuggiggle@hotmail.com>
2020-04-05 22:34:33 +0800

    text

--- ds&a/rb/rbtree.cc
+++ ds&a/rb/rbtree.cc
@@ -5,21 +5,27 @@
 using bfzq::rb_node_struct;
 using bfzq::RBTree;
 
-void visit1(const rb_node_struct<int> *tree)
+void visit1(const rb_node_struct<double> *tree)
 {
-  printf(".element=%d .color=%s\n",
+  printf(".element=%f .color=%s\n",
          tree->element, tree->color == bfzq::RED ? "RED" : "BLACK");
 }
 
 void test1()
 {
   printf("test1\n");
-  RBTree<int> *test = new RBTree<int>();
+  RBTree<double> *test = new RBTree<double>();
   test->insert(3);
   test->insert(2);
   test->insert(1);
+  test->insert(0);
+  test->insert(0.5);
+  test->insert(2.5);
+  test->insert(4);
+  test->insert(5);
+  test->insert(6);
   // test->insert(4);
-  print_tree<rb_node_struct<int>>(get_tree(*test), visit1);
+  print_tree<rb_node_struct<double>>(get_tree(*test), visit1);
   printf("\n");
   delete test;
 }
