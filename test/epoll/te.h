66cd75ea0156743efecb0a15be25bda5f9a3a853
bfzq <bigbuggiggle@hotmail.com>
2020-05-28 05:55:24 -0400

    code te

--- test/epoll/te.h
+++ test/epoll/te.h
@@ -1,7 +1,16 @@
 #ifndef _TE_H_
 #define _TE_H_
+struct te_event_t
+{
+  int tefd;
+  struct epoll_event *events;
+};
 
-int te_create();
+struct te_event_loop_t
+{
+  
+};
+int te_create(te_event_loop_t *event_loop);
 int te_free();
 int te_add_event();
 int te_del_event();
