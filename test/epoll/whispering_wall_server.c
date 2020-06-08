b09bad88a43154cb867c313cf9a9a2f8a10ad456
bfzq <bigbuggiggle@hotmail.com>
2020-06-08 06:03:10 -0400

    epoll

--- test/epoll/whispering_wall_server.c
+++ test/epoll/whispering_wall_server.c
@@ -1,6 +1,35 @@
-#include "te.h"
+/* for test epoll */
+// #include "te.h"
+#include <stdio.h>
 
-int main()
+#include <sys/epoll.h>
+
+int main(int argc, char* argv[])
 {
+  int listenfd = 0;
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in serveraddr;
+  bzero(&serveraddr, sizeof(struct sockaddr_in));
+  serveraddr.sin_famliy = AF_INET;
+  inet_aton("127.0.0.1", &(serveraddr.sin_addr));
+  serveraddr.sin_port = htons(5566);
+  bind(listenfd, (sockaddr*)&serveraddr, sizeof(serveraddr));
+  listen(listenfd, 20);
+
+  
+  int epfd; // for epoll
+  epfd = epoll_create(1024);
+  if (epfd < 0) return -1;
+
+  struct epoll_event ev;
+  ev.data.fd = listenfd;
+  ev.events = EPOLLIN | EPOLLOUT;
+  epoll_ctl(epfd, EPOLL_CTL_ADD, listenfd, &ev);
+
+  while (1)
+  {
+    // TODO
+  }
   return 0;
 }
