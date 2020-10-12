7054a6492700db1664d81ae6e96770bb9c108879
bfzq <bigbuggiggle@hotmail.com>
2020-10-12 18:20:23 +0800

    do not register sigbal function each time

--- test/fork.cc
+++ test/fork.cc
@@ -6,34 +6,35 @@
 
 #include <assert.h>
 
-int fork_proc();
+// int fork_proc();
 pid_t pid;
-void sigchld(int n)
-{
-  std::cout << "Get SIGCHLD" << n << std::endl;
-  // 当前进程在信号函数处理完成之后才会继续执行
-  // while(1)
-  // {
-  //   std::cout << "f : SIGCHLD" << n << std::endl;
-  //   sleep(2);
-  // }
+// void sigchld(int n)
+// {
+//   std::cout << "Get SIGCHLD" << n << std::endl;
+//   // 当前进程在信号函数处理完成之后才会继续执行
+//   // while(1)
+//   // {
+//   //   std::cout << "f : SIGCHLD" << n << std::endl;
+//   //   sleep(2);
+//   // }
 
-  // pid_t pid;
-  std::cout << "SIGCHLD c pid " << getpid() << std::endl;
-  int r = fork_proc();
-  if (r < 0)
-    return;
-  else
-  {
-    std::cout << "f : SIGCHLD new pid" << pid << std::endl;
-  }
-}
+//   // pid_t pid;
+//   std::cout << "SIGCHLD c pid " << getpid() << std::endl;
+//   int r = fork_proc();
+//   if (r < 0)
+//     return;
+//   else
+//   {
+//     std::cout << "f : SIGCHLD new pid" << pid << std::endl;
+//   }
+// }
 
+int fork_proc(bool register_sigal_function);
 void sigacchld(int n, siginfo_t *sig, void *p)
 {
   // 要让siginfo_t生效，需要设置SA_SIGINFO
   std::cout << "SIGCHLD child pid " << sig->si_pid << std::endl;
-  int r = fork_proc();
+  int r = fork_proc(false);
   if (r < 0)
     return;
   else
@@ -42,14 +43,14 @@ void sigacchld(int n, siginfo_t *sig, void *p)
   }
 }
 
-int fork_proc()
+int fork_proc(bool register_sigal_function)
 {
   pid = fork(); // 复制上下文，形成新的进程
   if (pid == 0)
   {
     // sub proc
   }
-  else if (pid > 0)
+  else if (pid > 0 && register_sigal_function == true)
   {
     struct sigaction act;
     act.sa_flags = SA_SIGINFO;
@@ -61,7 +62,7 @@ int fork_proc()
     sigemptyset(&mask);
     pthread_sigmask(SIG_SETMASK, &mask, NULL);
   }
-  else
+  else if (pid < 0)
   {
     return -1;
   }
@@ -70,7 +71,7 @@ int fork_proc()
 
 int main()
 {
-  int r = fork_proc();
+  int r = fork_proc(true);
   if (r < 0) return -1;
   while (1)
   {
