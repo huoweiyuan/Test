84e5ab83dc42fad6bcf05bdb8443b8b5ab699755
bfzq <huoweiyuan@foxmail.com>
2020-12-31 18:35:51 +0800

    max profit1

--- algorithms/leetcode.cc
+++ algorithms/leetcode.cc
@@ -13,11 +13,6 @@ TEST(LEETCODE, climbStairs0)
 {
   Leetcode leetcode;
   EXPECT_EQ(2,leetcode.climbStairs(2));
-}
-
-TEST(LEETCODE, climbStairs1)
-{
-  Leetcode leetcode;
   EXPECT_EQ(3,leetcode.climbStairs(3));
 }
 
@@ -25,18 +20,23 @@ TEST(LEETCODE, climbStairs1)
 TEST(LEETCODE, maxProfit0)
 {
   Leetcode leetcode;
-  vector<int> input = {7,1,5,3,6,4};
-  EXPECT_EQ(5, leetcode.maxProfit(input));
+  vector<int> input0 = {7,1,5,3,6,4};
+  EXPECT_EQ(5, leetcode.maxProfit(input0));
+  vector<int> input1 = {7,6,4,3,1};
+  EXPECT_EQ(0, leetcode.maxProfit(input1));
+  
 }
 
 TEST(LEETCODE, maxProfit1)
 {
   Leetcode leetcode;
-  vector<int> input = {7,6,4,3,1};
-  EXPECT_EQ(0, leetcode.maxProfit(input));
+  vector<int> input0 = {3,3,5,0,0,3,1,4};
+  EXPECT_EQ(6, leetcode.maxProfit1(input0));
+  vector<int> input1 = {1,2,3,4,5};
+  EXPECT_EQ(4, leetcode.maxProfit1(input1));
+  vector<int> input2 = {1,2,3,4,5};
+  EXPECT_EQ(0, leetcode.maxProfit1(input2));
 }
-
-
 // ====
 int main(int argc, char **argv)
 {
